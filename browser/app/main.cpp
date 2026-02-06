ed529cab920fa7dbf8777d02247a2f986efe6f96
DevByEagle <142937112+devbyeagle@users.noreply.github.com>
2026-02-06 03:46:20 +0000

    added if include statments

--- browser/app/main.cpp
+++ browser/app/main.cpp
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
+#if defined(_WIN32)
+#elif defined(__unix__)
+#endif
+
 int main(int argc, char* argv[]) {
-    printf("Hello, World!\n");
+    printf("Hello, World\n");
     return 0;
 }
